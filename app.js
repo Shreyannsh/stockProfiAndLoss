16d6bd43ade4affd2ad7968f858ac1f62a605d3b
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2022-09-23 19:59:46 +0530

    initial comment:
    wrote complete program.
    js file
    html file
    css file
    add background image

---
+++ app.js
@@ -0,0 +1,42 @@
+var initialPrice = document.querySelector("#initial-price");
+var quantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#current-price")
+var output = document.querySelector("#output");
+var button = document.querySelector("#button");
+
+function calculatingProfitAndLoss() {
+
+    var input1 = initialPrice.value;
+    var input2 = quantity.value;
+    var input3 = currentPrice.value;
+    if (input1 && input2 && input3 !== "") {
+        if (input3 > input1) {
+            profit = input3 - input1;
+            totalProfit = profit * input2;
+            profitPercentage = ((totalProfit / (input1 * input2)) * 100).toFixed(2);
+            output.innerText = `You are in profit of ${profitPercentage}% with ${totalProfit} rupees.
+            ~ "Great! Be invested"`
+            output.style.color = "green";
+        } else {
+            if (input1 > input3) {
+                loss = input1 - input3;
+                totalLoss = loss * input2;
+                lossPercentage = (totalLoss / (input3 * input2) * 100).toFixed(2);
+                output.innerText = `You are in loss of ${lossPercentage}% with ${totalLoss} rupees.
+            ~ "Re-evaluate your position"`
+                output.style.color = "maroon";
+            } else {
+                output.innerText = "Your are at neutral position without any profit or loss.\n Keep checking!"
+                output.style.color = "blue";
+            }
+        }
+
+    } else {
+        alert("Kindly enter valid values for all 3 entries");
+    }
+}
+
+
+
+
+button.addEventListener("click", calculatingProfitAndLoss);
